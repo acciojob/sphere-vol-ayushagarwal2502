cd9731e7a042217dbf49323f87f3761be4913e6c
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-01-13 01:39:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function volume_sphere() {
-    //Write your code here
-  
+    let r = document.getElementById("radius").value;
+    if (isNaN(r) || r < 0) {
+        document.getElementById('volume').value = 'NaN';
+    } else {
+        let v = (4/3) * Math.PI * Math.pow(r, 3);
+        document.getElementById('volume').value = v.toFixed(4);
+    }
 } 
-
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+ document.getElementById('MyForm').onsubmit = volume_sphere;
